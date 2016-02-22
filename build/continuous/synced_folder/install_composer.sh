cd81d2a484787cc4eab96867129c7e203a96e7c3
guy-mograbi-at-gigaspaces <guym@gigaspaces.com>
2016-02-22 14:46:49 +0200

    add prints

--- build/continuous/synced_folder/install_composer.sh
+++ build/continuous/synced_folder/install_composer.sh
@@ -2,7 +2,8 @@
 
 mkdir -p ~/composer
 pushd ~/composer
-    wget -qq -O composer.tar.gz $COMPOSER_URL
+    echo "composer URL is :: ${COMPOSER_URL}"
+    wget -qq -O composer.tar.gz ${COMPOSER_URL}
     tar -xzf composer.tar.gz
     echo " I am in `pwd`"
     ls -ll
