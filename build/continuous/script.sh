c31dca963ae3c446b2c6bcb9846bc7aea8611def
guy-mograbi-at-gigaspaces <guym@gigaspaces.com>
2016-03-10 12:03:14 +0200

    tru to move everything locally to avoid global collisions

--- build/continuous/script.sh
+++ build/continuous/script.sh
@@ -15,15 +15,12 @@ echo "user is $USER";
 echo "installing node"
 nvm install 0.10.35 # keep this in older version deliberately.
 
-# which json || npm install -g json
+which json || npm install -g json
 echo "GIT_REFERENCE is ${GIT_REFERENCE}"
 chmod 600 ${CONFIG_FILE}
-
-npm install json
-export JSON_BIN="./node_modules/json/lib/json.js"
-$JSON_BIN -I -f ${CONFIG_FILE} -e "this.environmentVariables.GIT_REFERENCE=\"${GIT_REFERENCE}\""
-$JSON_BIN -I -f ${CONFIG_FILE} -e "this.environmentVariables.TEST_TYPE=\"${TEST_TYPE}\""
-$JSON_BIN -I -f ${CONFIG_FILE} -e "this.environmentVariables.BROWSER_TYPE=\"${BROWSER_TYPE}\""
+json -I -f ${CONFIG_FILE} -e "this.environmentVariables.GIT_REFERENCE=\"${GIT_REFERENCE}\""
+json -I -f ${CONFIG_FILE} -e "this.environmentVariables.TEST_TYPE=\"${TEST_TYPE}\""
+json -I -f ${CONFIG_FILE} -e "this.environmentVariables.BROWSER_TYPE=\"${BROWSER_TYPE}\""
 
 
 ls -ll ${CONFIG_FILE}
@@ -34,9 +31,7 @@ curl -L https://goo.gl/j6qnth | INJECT_FILE="${CONFIG_FILE}" node
 chmod 600  ${PEM_FILE}
 
 
-npm install vagrant-automation-machines
-export VAGRANT_AUTOMATION_MACHINE_SETUP="./node_modules/vagrant-automation-machines/index.js"
-#which vagrant-automation-machines-copy || npm install cloudify-cosmo/vagrant-automation-machines -g
+which vagrant-automation-machines-copy || npm install cloudify-cosmo/vagrant-automation-machines -g
 
 function cleanup(){
     pushd ${VAGRANT_WORKDIR}
@@ -47,8 +42,7 @@ function cleanup(){
 trap cleanup EXIT
 
 pushd ${VAGRANT_BASEDIR}
-    $VAGRANT_AUTOMATION_MACHINE_SETUP aws
-#    vagrant-automation-machines-setup aws
+    vagrant-automation-machines-setup aws
     cleanup || echo "no need to teardown the machine because it was not running"
     pushd ${VAGRANT_WORKDIR}
         vagrant up --provider=aws
